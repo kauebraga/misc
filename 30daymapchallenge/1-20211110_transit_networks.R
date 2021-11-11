ca14854e701fbd5694f22b5fe9958d8fe5de842e
Kaue Braga <kauebraga90@gmail.com>
2021-11-11 14:31:29 -0300

    update

--- 30daymapchallenge/1-20211110_transit_networks.R
+++ 30daymapchallenge/1-20211110_transit_networks.R
@@ -51,3 +51,80 @@ ggplot()+
   
 
 (map_rio) / (map_cur + map_rec) /  (map_for + map_spo)  + plot_layout(widths = 1, heights = 1)
+
+
+
+
+
+
+
+# mesma escala --------------------------
+
+sfc_as_cols <- function(x, names = c("lon","lat")) {
+  stopifnot(inherits(x,"sf") && inherits(sf::st_geometry(x),"sfc_POINT"))                                                                                                                                                                                                                                                     
+  ret <- sf::st_coordinates(x)
+  ret <- tibble::as_tibble(ret)
+  stopifnot(length(names) == ncol(ret))
+  x <- x[ , !names(x) %in% names]
+  ret <- setNames(ret,names)
+  ui <- dplyr::bind_cols(x,ret)
+  st_set_geometry(ui, NULL)
+}
+# calcular centroides
+centroide_rio <- st_centroid(geobr::read_municipality(3304557)) %>% st_transform(4326) %>% sfc_as_cols()
+centroide_cur <- st_centroid(geobr::read_municipality(4106902)) %>% st_transform(4326) %>% sfc_as_cols()
+centroide_rec <- st_centroid(geobr::read_municipality(2611606)) %>% st_transform(4326) %>% sfc_as_cols()
+centroide_for <- st_centroid(geobr::read_municipality(2304400)) %>% st_transform(4326) %>% sfc_as_cols()
+centroide_spo <- st_centroid(geobr::read_municipality(3550308)) %>% st_transform(4326) %>% sfc_as_cols()
+
+
+# calcular bouningbox spo
+bbox_spo <- st_bbox(shapes_spo)
+bbox_rio <- st_bbox(shapes_rio)
+
+# calcular offset entre bbox e centroide de spo
+offset_xmin <- bbox_rio[[1]] - centroide_rio$lon 
+offset_xmax <- bbox_rio[[3]] - centroide_rio$lon 
+offset_ymin <- bbox_rio[[2]] - centroide_rio$lat
+offset_ymax <- bbox_rio[[4]] - centroide_rio$lat 
+
+
+
+map_rio_scale <-  
+  ggplot()+
+  geom_sf(data = shapes_rio, alpha = 0.2, size = 0.2) +
+  coord_sf(xlim = c(centroide_rio$lon + offset_xmin, centroide_rio$lon + offset_xmax),
+           ylim = c(centroide_rio$lat + offset_ymin, centroide_rio$lat + offset_ymax))+
+  theme_void()
+
+map_for_scale <- 
+  ggplot()+
+  geom_sf(data = shapes_for, alpha = 0.2, size = 0.2) +
+  coord_sf(xlim = c(centroide_for$lon + offset_xmin, centroide_for$lon + offset_xmax),
+           ylim = c(centroide_for$lat + offset_ymin, centroide_for$lat + offset_ymax))+
+  theme_void()
+
+map_cur_scale <- 
+  ggplot()+
+  geom_sf(data = shapes_cur, alpha = 0.2, size = 0.2) +
+  coord_sf(xlim = c(centroide_cur$lon + offset_xmin, centroide_cur$lon + offset_xmax),
+           ylim = c(centroide_cur$lat + offset_ymin, centroide_cur$lat + offset_ymax))+
+  theme_void()
+
+map_rec_scale <- 
+  ggplot()+
+  geom_sf(data = shapes_rec, alpha = 0.2, size = 0.2) +
+  coord_sf(xlim = c(centroide_rec$lon + offset_xmin, centroide_rec$lon + offset_xmax),
+           ylim = c(centroide_rec$lat + offset_ymin, centroide_rec$lat + offset_ymax))+
+  theme_void()
+
+map_spo_scale <- 
+  ggplot()+
+  geom_sf(data = shapes_spo, alpha = 0.2, size = 0.2) +
+  coord_sf(xlim = c(centroide_spo$lon + offset_xmin, centroide_spo$lon + offset_xmax),
+           ylim = c(centroide_spo$lat + offset_ymin, centroide_spo$lat + offset_ymax))+
+  theme_void()
+
+
+
+map_rio_scale + map_cur_scale + map_rec_scale + map_for_scale + map_spo_scale
